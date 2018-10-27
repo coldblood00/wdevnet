2963245354f40dfbd0c5e1871358c707c37a7b89
Mr.K <krushilnarola15@gmail.com>
2018-10-27 20:47:59 +0530

    Second

--- wdevnet/settings.py
+++ wdevnet/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'whome',
+    'wabout',
+    'wservice',
+    'wcontact',
+    'wwork',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +59,7 @@ ROOT_URLCONF = 'wdevnet.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +123,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
---
+++ wwork/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class WworkConfig(AppConfig):
+    name = 'wwork'
